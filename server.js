fb6124eb4e020071c705c436ae30bc1f0bb99ab1
rebosha <64796305+rebosha@users.noreply.github.com>
2025-03-12 21:58:21 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// základná odpoveď pre testovanie
+app.get('/', (req, res) => {
+  res.send('Server funguje!');
+});
+
+app.listen(port = 3000, () => {
+  console.log(`Server beží na porte ${port}`);
+});
